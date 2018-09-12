9c72c41fa0b82c5ed0cdaaa865fbe2a1e3b67bdd
Alex <a.gruk@innopolis.ru>
2018-09-12 22:40:30 +0300

    lab4 correction

--- Lab4/task1.c
+++ Lab4/task1.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
+
 int main() {
-	int n;
-  n = fork();
-  if (n == 0) {
-    printf("Hello from child [PID - %d]\n", getpid());
-  }
-  else {
-    printf("Hello from parent [PID - %d]\n", getpid());
-  }
-	return 0;
+
+int n;
+
+n = fork();
+
+if (n == 0){
+
+printf("Hello from child [PID - %d]\n", getpid());
+
+}
+  
+else {
+printf("Hello from parent [PID - %d]\n", getpid());
+
+}
+return 0;
 }
